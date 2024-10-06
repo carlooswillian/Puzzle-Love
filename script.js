9852513e24553b6c2a26ac84182b72e1500e6d35
carlooswillian <carloswsvieira@gmail.com>
2024-10-06 06:40:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ function resetPuzzle() {
 
 // Event listener para o botão de reinício
 document.getElementById('reset').addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex + 1) % images.length; // Avança para a próxima imagem
     resetPuzzle(); // Reinicia o quebra-cabeça
 });
 
